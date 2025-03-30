090fc7e29b51e954b238806965bef62f40bdc4da
Jihun Song <wlgns1080@naver.com>
2025-03-30 20:05:00 +0900

    * init 4dgs
    - init 4dgs

---
+++ scene/gaussian_model.py
@@ -0,0 +1,36 @@
+class GaussianModel:
+    def setup_functions(self):
+        def build_covariance_from_scaling_rotation(scaling, scaling_modifier, rotation):
+            L = build_scaling_rotation(scaling_modifier * scaling, rotation)
+            # build scaling rotation -> scaling modifier var 를 이용해서 scaling
+            actual_covariance = L.transpose(1, 2) @ L
+            symm = strip_symmetric(actual_covariance)
+            return symm
+        
+        def build_covariance_from_scaling_rotation_4d(scaling, scaling_modifier, rotation_l, rotation_r, dt=0.0):
+            L = build_scaling_rotation_4d(scaling_modifier * scaling, rotation_l, rotation_r)
+            actual_covariance = L @ L.transpose(1, 2)
+            cov_11 = actual_covariance[:,:3,:3]
+            cov_12 = actual_covariance[:,0:3,3:4]
+            cov_t = actual_covariance[:,3:4,3:4]
+            current_covariance= cov_11 - cov_12 @ cov_12.transpose(1, 2) / cov_t
+            symm = strip_symmetric(current_covariance)
+            if dt.shape[1] > :
+                mean_offset = (cov_12.squeeze(-1) / cov_t.squeeze(-1))[:, None, :] * dt[..., None]
+                mean_offset = mean_offset[..., None]  # [num_pts, num_time, 3, 1]
+            else:
+                mean_offset = cov_12.squeeze(-1) / cov_t.squeeze(-1) * dt
+            return symm, mean_offset.squeeze(-1)
+        
+        self.scaling_activation = torch.exp
+        self.scaling_inverse_activation = torch.log
+
+        if not self.rot_4d:
+            self.covariance_activation = build_covariance_from_scaling_rotation
+        else:
+            self.covariance_activation = build_covariance_from_scaling_rotation_4d
+
+        self.opacity_activation = torch.sigmoid
+        self.inverse_opacity_activation = inverse_sigmoid
+
+        self.rotation_activation = torch.nn.functional.normalize
